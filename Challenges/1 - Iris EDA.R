34776ed7c1082436e78866913b6d55cc2a134802
Barbara Fusinska <barbara.fusinska@gmail.com>
2016-05-11 21:16:50 +0100

    Added Iris EDA challenge code

---
+++ Challenges/1 - Iris EDA.R
@@ -0,0 +1,48 @@
+# Set path to the iris data file
+irisPath <- 'Data/iris.data'
+
+# Read data from file
+irisData <- read.csv(irisPath, header = FALSE)
+
+# Set proper headers
+names(irisData)
+
+names(irisData) <- c('Sepal.l', 'Sepal.w', 'Petal.l', 'Petal.w', 'Species')
+
+# Have a look at the data
+names(irisData)
+head(irisData)
+
+summary(irisData)
+
+# Add petal and sepal areas
+irisData$Sepal.Area = irisData$Sepal.l * irisData$Sepal.w
+irisData$Petal.Area = irisData$Petal.l * irisData$Petal.w
+
+head(irisData)
+
+# Change column order
+irisData <- irisData[, c(1,2,3,4,6,7,5)]
+head(irisData)
+
+# Plot sepal with petal areas
+
+irisData$Colour[irisData$Species == 'Iris-setosa'] = 1
+irisData$Colour[irisData$Species == 'Iris-versicolor'] = 2
+irisData$Colour[irisData$Species == 'Iris-virginica'] = 3
+
+plot(irisData$Sepal.Area, irisData$Petal.Area, xlab = 'Sepal length', 
+     ylab = 'Sepal width', main = 'Irises', col = irisData$Colour)
+
+irisData$Colour <- NULL
+
+#-------------------------------------------------
+# Use datasets for iris data
+str(iris)
+
+attach(iris)
+head(iris)
+detach(iris)
+
+# List available datasets
+data()
