1b143e233b70dd3e3b63edbbac2562ec15db8833
Barbara Fusinska <barbara.fusinska@gmail.com>
2016-05-11 21:32:44 +0100

    Added 1 - Basic types code

---
+++ Tutorial/1 Basic Types.R
@@ -0,0 +1,54 @@
+# Numeric type
+numericVariable <- 5
+numericVariable
+typeof(numericVariable)
+
+numericVector <- c(1,2,3,4,5)
+numericVector
+typeof(numericVector)
+
+# Text 
+textVariable <- 'abcdef'
+textVariable
+typeof(textVariable)
+
+textVector <- c('a', 'b', 'c', 'd', 'e')
+textVector
+typeof(textVector)
+
+# Logical type
+boolVariable <- TRUE
+boolVariable
+typeof(boolVariable)
+
+boolVector <- c(TRUE, FALSE, FALSE, TRUE, FALSE)
+boolVector
+typeof(boolVector)
+
+# Dynamic types
+numericVariable <- FALSE
+numericVariable
+typeof(numericVariable)
+
+# Vector elements
+numericVector[1]
+textVector[2]
+boolVector[3]
+
+# 0-index
+numericVector[0]
+textVector[0]
+boolVector[0]
+
+# Default values
+defaultNumeric <- double(10)
+defaultNumeric
+defaultText <- character(10)
+defaultText
+defaultBool <- logical(10)
+defaultBool
+
+# Generating values
+1:20
+c(4, 10:15, 22)
+letters[3:9]
