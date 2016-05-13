6f39e530d3a549acce23c3efc8627bf574e63a10
Barbara Fusinska <barbara.fusinska@gmail.com>
2016-05-13 21:15:01 +0100

    Moved plots code to separate folder

--- Tutorial/2 I-O/2.4 Writing to a file.R
+++
@@ -1,8 +0,0 @@
-# Write data frame to a file
-outputPath <- 'Data/sizesOutput.csv'
-
-write.csv(sizes, file=outputPath)
-sizes
-
-# Without row names
-write.csv(sizes, file=outputPath, row.names = FALSE)
