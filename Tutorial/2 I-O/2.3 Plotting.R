be073fb8cc43f8923e29edfcbd06d07a00a28236
Barbara Fusinska <barbara.fusinska@gmail.com>
2016-05-13 14:57:17 +0100

    Added Plotting tutorial code

---
+++ Tutorial/2 I-O/2.3 Plotting.R
@@ -0,0 +1,10 @@
+# Drawing points
+plot(sizes$length, sizes$width)
+
+# Labels
+plot(sizes$length, sizes$width, xlab = 'Lenth', ylab = 'Width')
+
+# Changing colour
+plot(sizes$length, sizes$width, xlab = 'Lenth', ylab = 'Width', col = 2)
+plot(sizes$length, sizes$width, xlab = 'Lenth', ylab = 'Width', col = 3)
+plot(sizes$length, sizes$width, xlab = 'Lenth', ylab = 'Width', col = 4)
